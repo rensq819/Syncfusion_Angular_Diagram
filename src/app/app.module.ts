ef3a4879dbc771f7850d1408dea677d568073047
rensq819 <siqi.ren@gmail.com>
2018-06-05 15:24:50 -0400

    added header and removed route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DiagramComponent } from './diagram/diagram.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { FaqComponent } from './faq/faq.component';
+import { HeaderComponent } from './header/header.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
@@ -14,7 +15,8 @@ import { MaterialModule } from './material.module';
   declarations: [
     AppComponent,
     DiagramComponent,
-    FaqComponent  
+    FaqComponent,
+    HeaderComponent  
   ],
   imports: [
     BrowserModule, 
--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -21,7 +21,6 @@ export class FaqComponent implements OnInit {
     this.http.get('http://localhost:3000/api/faq')
     .subscribe(data => {
       let temp = data[0] as Array<any>;
-      debugger
       this.content = Object.values(temp["children"]);   
       this.faq = Object.values(this.content[0]["children"]); 
     })
